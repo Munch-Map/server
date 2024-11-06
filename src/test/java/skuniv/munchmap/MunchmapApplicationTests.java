39cec1c95468dba8a0ea5bbbbcec3de454017f46
SooD <dlatnqls921@naver.com>
2024-11-06 22:50:47 +0900

    init

---
+++ src/test/java/skuniv/munchmap/MunchmapApplicationTests.java
@@ -0,0 +1,13 @@
+package skuniv.munchmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunchmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
